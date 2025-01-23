2fe04ecf3e147602cea532808b767e67b2de44d7
ZorialCart <zorialcart@outlook.com>
2025-01-23 14:30:47 -0700

    App terminada

---
+++ MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿namespace MauiApp_MGCC
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            getRes();  
+        }
+
+        public void getRes()
+        {
+            double imc;
+            if ((double.TryParse(EntryAlt.Text, out double inputAlt)) && (double.TryParse(EntryPes.Text, out double inputPes)))
+            {
+                imc = inputPes / Math.Pow(inputAlt, 2);
+
+                if (imc < 18.5)
+                    RIMC.Text = "Estás en el peso normal.";
+
+                if (imc >= 18.5 && imc <= 24.9)
+                    RIMC.Text = "Estás en el peso normal.";
+
+                if (imc >= 25 && imc <= 29.9)
+                    RIMC.Text = "Tienes en el peso normal.";
+
+                if (imc > 30)
+                    RIMC.Text = "Tienes sobrepeso.";
+
+                Result.Text = imc.ToString();
+
+
+            }
+
+
+
+
+
+
+
+
+            //if (imc < 18.5)
+            //{
+            //   Console.WriteLine("El peso es bajo.");
+            //}   
+            //if(imc )
+            //    18.5 BAJO
+            //18.5 - 24.9     nORMAL
+
+            //25 - 29.9
+            //30
+
+
+
+        }
+
+    }
+
+}
+
+
+
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiApp_MGCC
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
